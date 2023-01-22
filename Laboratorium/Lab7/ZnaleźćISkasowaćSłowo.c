92c3e48f689df2a190882804c69bd2a296481639
SebasPav <115025994+SebasPav@users.noreply.github.com>
2023-01-22 19:16:07 +0200

    Update ZnaleźćISkasowaćSłowo.c

--- Laboratorium/Lab7/ZnaleźćISkasowaćSłowo.c
+++ Laboratorium/Lab7/ZnaleźćISkasowaćSłowo.c
@@ -1,6 +1,6 @@
 // Sebastian Pavlovič
 // Tablica
-// 2023-01-17
+// 2023-01-22
 
 #include <stdio.h>
 
@@ -13,14 +13,17 @@ int main(void) {
       "reiciendis ea dignissimos cupiditate dolores autem. Error mollitia sed "
       "soluta. Eos autem nostrum consequatur quo eligendi cumque. Nihil "
       "aliquam magnam atque autem est quia optio voluptatum voluptatibus.";
+  
   int len = sizeof(text) / sizeof(text[0]);
   int max = max_dlugosc_slowa(text, len);
 
   for (int i = 0; i < len; i++) {
-    printf("%d ", text[i]);
+    printf("%c", text[i]);
   }
   printf("\n");
+  
   brute_force_remove(text, len, max);
+  
   printf("\n");
   for (int i = 0; i < len; i++) {
     printf("%c", text[i]);
@@ -46,17 +49,20 @@ int max_dlugosc_slowa(char *text, int len) {
 
 void brute_force_remove(char *text, int len, int max) {
   int tak;
+  
   for (int i = 0; i < len; i++) {
     tak = 0;
+    
     for (int j = 0; j < max; j++) {
       if (text[i + j] != 32) {
         tak = 100;
       }
     }
+    
     if (tak == 100) {
       if (i + max < len) {
         for (int j = 0; i + j < len; j++) {
-          text[i + j] = text[i + j + max];
+          text[i + j] = text[i + j + 1];
         }
       }
     }
